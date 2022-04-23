63ddc1994d194fad9ff265c5555ee2fcd0e0f6b6
Thomas Ferreira <weltonthomasferreira@outlook.com>
2022-04-23 15:10:42 -0300

    feat(req4): implements the route to register a car

--- src/controllers/RegisterCarController.ts
+++ src/controllers/RegisterCarController.ts
@@ -1,29 +1,33 @@
+import { serverError, created, badRequest } from '../helpers/httpHelpers';
 import { Car } from '../interfaces/CarInterface';
+import { IResponse } from '../interfaces/ResponseInterface';
 import { Model } from '../interfaces/ModelInterface';
+import { Service } from '../interfaces/ServiceInterface';
+import { IController } from '../interfaces/ControllerInterface';
 
 export interface IRequest {
   body: Car;
 }
 
-export interface IResponse {
-  statusCode: number;
-  body: unknown;
-}
-
-export class RegisterCarController {
+export default class RegisterCarController implements IController<IRequest> {
   private readonly _carModel;
 
-  private readonly _service: unknown;
+  private readonly _carValidation;
 
-  constructor(model: Model<Car>, service: unknown) {
+  constructor(model: Model<Car>, service: Service<Car>) {
     this._carModel = model;
-    this._service = service;
+    this._carValidation = service;
   }
 
   async handle(req: IRequest): Promise<IResponse> {
-    const response = await this._carModel.create(req.body);
-    return { statusCode: 201, body: response };
+    try {
+      const error = await this._carValidation.validate(req.body);
+      console.log(error);
+      if (error) return badRequest(error);
+      const response = await this._carModel.create(req.body);
+      return created(response);
+    } catch (error) {
+      return serverError(error as Error);
+    }
   }
 }
-
-export default { RegisterCarController };
--- src/routers/carsRouter.ts
+++ src/routers/carsRouter.ts
@@ -1,10 +1,14 @@
 import { Router, Request, Response } from 'express';
-import { RegisterCarController } from '../controllers/RegisterCarController';
-import { CarModel } from '../models/CarModel';
+import RegisterCarController from '../controllers/RegisterCarController';
+import CarModel from '../models/CarModel';
+import CarValidator from '../services/CarValidator';
 
 const carsRouter = Router();
 
-const registerCarController = new RegisterCarController(new CarModel(), {});
+const registerCarController = new RegisterCarController(
+  new CarModel(),
+  new CarValidator(),
+);
 
 carsRouter.post('/cars', async (req: Request, res: Response) => {
   const response = await registerCarController.handle(req);
