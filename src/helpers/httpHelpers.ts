ec1ca83828e4bbbbe3a61b5f41c9d94fe2a61a54
Thomas Ferreira <weltonthomasferreira@outlook.com>
2022-04-24 20:40:39 -0300

    feat(helpers): create ok response

--- src/helpers/httpHelpers.ts
+++ src/helpers/httpHelpers.ts
@@ -15,3 +15,8 @@ export const created = (data: unknown): IResponse => ({
   statusCode: 201,
   body: data,
 });
+
+export const ok = (data: unknown): IResponse => ({
+  statusCode: 200,
+  body: data,
+});
